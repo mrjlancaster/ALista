04bb78dc29d158e89d880fe9c375388a9c04bab3
mrjlancaster <lancast001@gmail.com>
2021-08-06 00:13:36 +0200

    add transition

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
-import "./Body.css";
+import "./home.css";
 
 const Home = ({ registrationEmail, setRegistrationEmail }) => {
 	return (
