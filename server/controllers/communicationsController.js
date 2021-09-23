78510a69f51834dbece4fb89772a10b158d42cd9
mrjlancaster <lancast001@gmail.com>
2021-09-23 00:08:15 -0500

    create api for newsletter email

--- server/controllers/communicationsController.js
+++ server/controllers/communicationsController.js
@@ -1,10 +1,34 @@
+const pool = require("../db");
+
 const notify = async (req, res) => {
 	const { email } = req.body;
 
 	try {
-		console.log(email);
+		// Validate data
+		if (!email || !email.includes("@"))
+			throw Error("Please use a valid email");
+
+		// Check if email is already stored
+		const prospect = await pool.query(
+			"SELECT * FROM newsletter WHERE email = $1",
+			[email]
+		);
+
+		if (prospect.rows.length && prospect.rows[0].email === email)
+			throw Error("User already exists");
+
+		const newProspect = await pool.query(
+			"INSERT INTO newsletter (email) VALUES ($1) RETURNING *",
+			[email]
+		);
+
+		res.status(200).json({
+			success: true,
+			email: newProspect.rows[0].email,
+			message: "Data received",
+		});
 	} catch (error) {
-		console.log(error);
+		res.status(400).json({ success: false, message: error.message });
 	}
 };
 
